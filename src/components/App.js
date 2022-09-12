3c2943cca4c427543e25dc3fc91cd94ed33e6d25
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-12 01:15:27 -0300

    add storage + routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
+import {useState} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 import {ThemeProvider} from "styled-components";
 import {GlobalStyle} from "./styles/GlobalStyle.sc";
+import GlobalContext from "./contexts/GlobalContext";
+import {getMyWalletUser} from "../services/storage";
 
 export default function App() {
+  const [login, setLogin] = useState(getMyWalletUser());
   const theme = {
     purple: "#8C11BE",
     violet: "#A328D6",
@@ -16,6 +22,17 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <GlobalContext.Provider value={{login, setLogin, theme}}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            {/*<Route path="/sign-up" element={<SignUp />} />*/}
+            {/*<Route path="/my-wallet" element={<MyWallet />} />*/}
+            {/*<Route path="/balance/new/:type" element={<NewBalance />} />*/}
+            {/*<Route path="/balance/edit/:type" element={<EditBalance />} />*/}
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </ThemeProvider>
   );
 }
